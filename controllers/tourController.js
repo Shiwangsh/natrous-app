36b0d7e9189cbc38ed5062b1429a5d551f30c75b
Shiwangsh Kc <Shiwangshkc@gmail.com>
2022-09-04 15:59:47 +0545

    API query features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,67 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Query params storing as a hardcopy
+    console.log(req.query);
+
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    //Removing selected fields from the queryobj
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    //gte, gt, lte, lt
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      // console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //Field Limiting
+    if (req.query.feilds) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limt * 1 || 100;
+
+    // page=2&limit=10 -> 11-20, page=3&limit=10 -> 21-30
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page dos not exist');
+    }
+
+    //Execute query
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
